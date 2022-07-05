726691b258005e701253a6edd29122a48efdf65c
Luis Fernando <luis.silva9902@gmail.com>
2022-07-05 19:06:44 -0300

    Prismic API (CMS) connection and configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,28 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { PrismicProvider } from '@prismicio/react'
+import { linkResolver, repositoryName } from '../services/prismicio'
+import Link from 'next/link'
+import { PrismicPreview } from '@prismicio/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props}) => (
+        <Link href={href}>
+          <a {...props}>
+            {children}
+          </a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Header />
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>    
   )
 }
 
