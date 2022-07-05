412abc56f757a9ec636e97d4ea94ff982f43cc02
Luis Fernando <luis.silva9902@gmail.com>
2022-07-04 21:30:57 -0300

    Add Next Link in posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
@@ -15,7 +16,9 @@ const Home: NextPage = () => {
           <li className={styles.post}>
             <img src="https://pbs.twimg.com/profile_images/1004666132253507584/gTEhhS7D_400x400.jpg" alt="" />
             <div>
-              <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              <Link href='/'>
+                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              </Link>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
             </div>
           </li>
@@ -23,7 +26,9 @@ const Home: NextPage = () => {
           <li className={styles.post}>
             <img src="https://ichef.bbci.co.uk/news/640/cpsprodpb/133CB/production/_103759787_gettyimages-949493748.jpg" alt="" />
             <div>
-              <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              <Link href='/'>
+                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              </Link>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
             </div>
           </li>
@@ -31,7 +36,9 @@ const Home: NextPage = () => {
           <li className={styles.post}>
             <img src="https://upload.wikimedia.org/wikipedia/pt/9/9f/RedeTV_News.jpg" alt="" />
             <div>
-              <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              <Link href='/'>
+                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              </Link>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
             </div>
           </li>
@@ -39,7 +46,9 @@ const Home: NextPage = () => {
           <li className={styles.post}>
             <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/breaking-news-poster-design-template-d020bd02f944a333be71e17e3a38db24_screen.jpg" alt="" />
             <div>
-              <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              <Link href='/'>
+                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
+              </Link>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
             </div>
           </li>
