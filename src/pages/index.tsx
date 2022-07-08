3bcb3b4604ccac6559e0a5996fffd355d9294405
Luis Fernando <luis.silva9902@gmail.com>
2022-07-08 18:08:58 -0300

    Dynamic post route configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,53 +36,13 @@ const Home: NextPage<HomeProps> = ({results}) => {
             <li className={styles.post} key={post.id}>
               <img src={post.data.banner} alt="Banner do post" />
               <div>
-                <Link href='/'>
+                <Link href={`/post/${post.uid}`}>
                   <h1>{post.data.title}</h1>
                 </Link>
                 <p>{post.data.contentPreview}...</p>
               </div>
             </li>
           ))}
-
-          {/* <li className={styles.post}>
-            <img src="https://pbs.twimg.com/profile_images/1004666132253507584/gTEhhS7D_400x400.jpg" alt="" />
-            <div>
-              <Link href='/'>
-                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
-              </Link>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
-            </div>
-          </li> */}
-
-          {/* <li className={styles.post}>
-            <img src="https://ichef.bbci.co.uk/news/640/cpsprodpb/133CB/production/_103759787_gettyimages-949493748.jpg" alt="" />
-            <div>
-              <Link href='/'>
-                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
-              </Link>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
-            </div>
-          </li>
-
-          <li className={styles.post}>
-            <img src="https://upload.wikimedia.org/wikipedia/pt/9/9f/RedeTV_News.jpg" alt="" />
-            <div>
-              <Link href='/'>
-                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
-              </Link>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
-            </div>
-          </li>
-
-          <li className={styles.post}>
-            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/breaking-news-poster-design-template-d020bd02f944a333be71e17e3a38db24_screen.jpg" alt="" />
-            <div>
-              <Link href='/'>
-                <h1>Descoberta comunidade de hackers formada por adolescentes</h1>
-              </Link>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque natus perferendis beatae? Harum aperiam libero explicabo placeat nobis obcaecati sunt recusandae, labore similique, quis eum quam mollitia. Nisi, quidem totam.</p>
-            </div>
-          </li> */}
         </ul>
       </main>
     </div>
---
+++ src/pages/post/[slug].tsx
@@ -0,0 +1,93 @@
+import { GetStaticPaths, GetStaticProps } from "next"
+import Head from "next/head"
+import { createClient } from "../../services/prismicio"
+
+interface PostProps {
+  results: {
+    id: string;
+    uid: string;
+    createdAt: string;
+    data: {
+      title: string;
+      contentPreview: string;
+      author: string;
+      banner: string;
+      content: {
+        subtitle: string;
+        paragraph: string;
+      }[]
+    }
+  }[]
+}
+
+export default function Post({ results }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{results[0].data.title}</title>
+      </Head>
+      <main>
+        {results.map(post => (
+          <div>
+            <img src={post.data.banner} alt="Banner do post" />
+
+            <h1>{post.data.title}</h1>
+            <span>{post.data.author}</span>
+            <span>{post.createdAt}</span>
+
+            <section>
+              {post.data.content.map(content => (
+                <>
+                  {content.subtitle && <h2>{content.subtitle}</h2>}
+                  <p>{content.paragraph}</p>
+                </>
+              ))}
+            </section>
+            
+          </div>
+        ))}
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking"
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params, previewData }) => {
+
+  const client = createClient({ previewData })
+  const postUID = params?.slug?.toString()
+
+  const post = await client.getByUID('post', postUID ? postUID : '')
+
+  const results = [
+    {
+      id: post.id,
+      uid: post.uid,
+      createdAt: post.first_publication_date,
+      data: {
+        title: post.data.title[0].text,
+        contentPreview: post.data.content[0].paragraph[0].text.substring(0, 200),
+        author: post.data.author[0].text,
+        banner: post.data.banner.url,
+        content: post.data.content.map((content: any) => {
+          return {
+            subtitle: content.subtitle[0] ? content.subtitle[0].text : '',
+            paragraph: content.paragraph[0].text
+          }
+        })
+      }
+    }
+  ]
+
+  return {
+    props: {
+      results
+    }
+  }
+}
